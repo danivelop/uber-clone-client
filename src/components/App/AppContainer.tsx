474ba2fb5018a238e544535a6a69072418bc3c17
danivelop <mofg266@gmail.com>
2020-02-21 06:30:26 +0900

    implement login, phoneLogin pages

--- src/components/App/AppContainer.tsx
+++ src/components/App/AppContainer.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import GlobalStyle from '../../global-styles';
+import { theme } from '../../theme';
+import { ThemeProvider } from '../../typed-components';
 import AppPresenter from './AppPresenter';
 import { IS_LOGGED_IN } from './AppQueries';
 
 const AppContainer: any = (props) => (
-  <AppPresenter isLoggedIn={props.data.auth.isLoggedIn} />
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <AppPresenter isLoggedIn={props.data.auth.isLoggedIn} />
+    </ThemeProvider>
+    <ToastContainer draggable={true} position="bottom-center" />
+  </>
 );
 
 export default graphql(IS_LOGGED_IN)(AppContainer)
